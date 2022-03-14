f4cef83a74951a166b18cb00e166da2849721a72
deleko <miguel.dlk@gmail.com>
2022-03-14 21:53:30 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -22,7 +22,7 @@ def start(update, context):
     logger.info(f'{name} sent /start')
     context.bot.sendMessage(chat_id=user_id,
                             parse_mode="HTML",
-                            text=f"Hola <b>{name}</b>, con /help verás todo lo que puedo hacer."
+                            text=f"Hola <b>{name}</b>, con /help verás todo lo que puedes enviarme"
                             )
 
 
@@ -32,7 +32,7 @@ def help(update, context):
     logger.info(f'{name} sent /help')
     context.bot.sendMessage(chat_id=user_id,
                             parse_mode="HTML",
-                            text=f"- Mándame la ubicación para saber las gasolineras cercanas. \n - Mándame un link de amazon (y compra con mi link) para invitarme a un café ☕."
+                            text=f"- Ubicación para saber las gasolineras cercanas \n\n- Link de amazon <i>(y compra con mi link)</i> para invitarme a un café ☕"
                             )
 
 
