dd2374fb9dd13cd56f094635d5c48020a5853f97
deleko <miguel.dlk@gmail.com>
2022-03-14 21:16:41 +0100

    Update gas.py

--- gas_api/gas.py
+++ gas_api/gas.py
@@ -20,17 +20,19 @@ def carga_gasolineras():
 def busca_gasolineras(user_lat, user_lon):
     lista = []
     for i in range(len(estaciones_list)):
-        gaslat = estaciones_list[i]['Latitud'].replace(',', '.')
-        gaslon = estaciones_list[i]['Longitud (WGS84)'].replace(',', '.')
-        distanciakm = distancia(user_lat, user_lon, gaslat, gaslon)
-        rotulo = estaciones_list[i]["Rótulo"]
-        diesel = estaciones_list[i]["Precio Gasoleo A"]
-        gasolina95 = estaciones_list[i]["Precio Gasolina 95 E5"]
-        gasolina98 = estaciones_list[i]["Precio Gasolina 98 E5"]
-        if distanciakm < 3 and len(lista) < 5:
-            msg = f"⛽ <b>{rotulo}</b> \n" \
-                  f"- Diesel: {diesel}€ \n" \
-                  f"- Gasolina95: {gasolina95}€ \n" \
-                  f"📍 <a href='https://maps.google.com/maps?q={gaslat},{gaslon}'>Google Maps</a> {round(distanciakm, 2)} Km \n\n\n"
-            lista.append(msg)
+        gaslat = float(estaciones_list[i]['Latitud'].replace(',', '.'))
+        gaslon = float(estaciones_list[i]['Longitud (WGS84)'].replace(',', '.'))
+        pasar = gaslat > user_lat -0.15 and gaslat < user_lat + 0.15 and gaslon > user_lon -0.15 and gaslon < user_lon + 0.15        
+        if pasar:
+            distanciakm = distancia(user_lat, user_lon, gaslat, gaslon)
+            rotulo = estaciones_list[i]["Rótulo"]
+            diesel = estaciones_list[i]["Precio Gasoleo A"]
+            gasolina95 = estaciones_list[i]["Precio Gasolina 95 E5"]
+            gasolina98 = estaciones_list[i]["Precio Gasolina 98 E5"]
+            if distanciakm < 3 and len(lista) < 5:
+                msg = f"⛽ <b>{rotulo}</b> \n" \
+                      f"- Diesel: {diesel}€ \n" \
+                      f"- Gasolina95: {gasolina95}€ \n" \
+                      f"📍 <a href='https://maps.google.com/maps?q={gaslat},{gaslon}'>Google Maps</a> {round(distanciakm, 2)} Km \n\n\n"
+                lista.append(msg)
     return lista
